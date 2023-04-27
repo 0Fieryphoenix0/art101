7c28ffbfa8bc8ae73e2c02e27bcec6223b4f3866
0Fieryphoenix0 <129906764+0Fieryphoenix0@users.noreply.github.com>
2023-04-26 21:27:33 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,9 +1,13 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Lily Zhao
+// Date: 4/26/2023
 
 // Constants
-
+make = "Volvo";
+model = "Unknown";
+year = 2023;
+ownIt = false;
+age = 2023 - year;
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
@@ -15,6 +19,11 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Own It: " + ownIt + "<br>");
+  document.writeln("Age: " + age + "<br>");
 }
 
 // let's get this party started
