865e252b025575b184e553f055ad6dc16fd72ec6
0Fieryphoenix0 <129906764+0Fieryphoenix0@users.noreply.github.com>
2023-05-08 22:17:30 -0700

    L8

---
+++ lab8/js/lab.js
@@ -0,0 +1,15 @@
+// index.js - To print out my main way of transportation
+// Author: Lily Zhao
+// Date:5/8/2023
+
+// Constants
+
+// Functions
+
+function square(x){
+  var results = x * x;
+  return results;
+}
+array = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,100,31,67];
+
+document.writeln("updated array", array.map(square),"<br>");
